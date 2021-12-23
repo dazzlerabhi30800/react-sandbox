6fb8e6f64c8273a7495b39b640e2bb338afb9a9f
= <abhisingh30800@gmail.com>
2021-12-23 12:23:36 +0530

    Use custom hook example 2 UselocalStorage

---
+++ src/components/CustomHookExample2.jsx
@@ -0,0 +1,43 @@
+// import {useState} from 'react'
+import useLocalStorage from '../hooks/useLocalStorage'
+
+function CustomHookExample2() {
+    const [task, setTask] = useLocalStorage('task', '')
+    const [tasks, setTasks] = useLocalStorage('tasks', [])
+
+    const onSubmit = e => {
+        e.preventDefault()
+
+        const taskObj = {
+            task,
+            completed : false,
+            date : new Date().toLocaleDateString()
+        }
+
+        setTasks([...tasks, taskObj])
+    }
+
+    return (
+        <>
+       <form onSubmit={onSubmit} className="w-50">
+           <div className="mb-3">
+               <label className="form-label">Task</label>
+               <input
+                type="text"
+                className='form-control'
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+                
+                />
+           </div>
+               <button type='submit' className="btn my-2 btn-primary">Submit</button>
+       </form>
+       <hr />
+       {tasks.map((task, index) => (
+           <h3 key={index}>{task.task}</h3>
+       ))}
+        </>
+    )
+}
+
+export default CustomHookExample2
